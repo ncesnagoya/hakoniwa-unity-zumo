30e0c250d1f39eff5ed0654ef4270f316c027830
Takashi Mori <kanetugu2015@gmail.com>
2023-09-06 11:28:24 +0900

    add motor

---
+++ zumo/Assets/Scripts/HakoAssets/HakoAssets/PluggableAsset/Assets/Robot/Zumo/ZumoImuTestDriver.cs
@@ -0,0 +1,106 @@
+using Hakoniwa.PluggableAsset.Assets.Robot.Parts;
+using Hakoniwa.PluggableAsset.Communication.Connector;
+using Hakoniwa.PluggableAsset.Communication.Pdu;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Hakoniwa.PluggableAsset.Assets.Robot.Zumo
+{
+    public class ZumoImuTestDriver : MonoBehaviour, IRobotPartsSensor
+    {
+        private GameObject root;
+        private PduIoConnector pdu_io;
+        private IPduWriter pdu_writer;
+        private string root_name;
+        public string topic_name = "sensor";
+        public string roboname = "ZumoRoboModel";
+        private TextMeshPro testMeshPro;
+
+        public RosTopicMessageConfig[] getRosConfig()
+        {
+            return new RosTopicMessageConfig[0];
+        }
+
+        public void Initialize(object obj)
+        {
+            GameObject tmp = null;
+            try
+            {
+                tmp = obj as GameObject;
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.LogError("Initialize error: " + e.Message);
+                return;
+            }
+
+            if (this.root == null)
+            {
+                this.root = tmp;
+                this.root_name = string.Copy(this.root.transform.name);
+                this.pdu_io = PduIoConnector.Get(roboname);
+                if (this.pdu_io == null)
+                {
+                    throw new ArgumentException("can not found pdu_io:" + root_name);
+                }
+                var pdu_writer_name = roboname + "_" + this.topic_name + "Pdu";
+                this.pdu_writer = this.pdu_io.GetWriter(pdu_writer_name);
+                if (this.pdu_writer == null)
+                {
+                    throw new ArgumentException("can not found pdu_writer:" + pdu_writer_name);
+                }
+                this.testMeshPro = this.GetComponentInChildren<TextMeshPro>();
+                if (this.testMeshPro == null)
+                {
+                    throw new ArgumentException("can not found TextMeshPro");
+                }
+
+            }
+        }
+
+        private Vector3 acc;
+        private Vector3 mag;
+        private Vector3 gyro;
+        public bool isAttachedSpecificController()
+        {
+            return false;
+        }
+
+        public void UpdateSensorValues()
+        {
+            this.acc = new Vector3(
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("acc").GetDataFloat64("x"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("acc").GetDataFloat64("y"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("acc").GetDataFloat64("z")
+            );
+            this.mag = new Vector3(
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("mag").GetDataFloat64("x"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("mag").GetDataFloat64("y"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("mag").GetDataFloat64("z")
+            );
+            this.gyro = new Vector3(
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("gyro").GetDataFloat64("x"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("gyro").GetDataFloat64("y"),
+                (float)this.pdu_writer.GetReadOps().Ref("imu").Ref("gyro").GetDataFloat64("z")
+            );
+
+        }
+
+        void Update()
+        {
+            string mag = "mag:";
+            string gyro = "gyro:";
+            string acc = "acc:";
+            this.testMeshPro.text = mag + this.mag.ToString();
+            this.testMeshPro.text += "\n";
+            this.testMeshPro.text += gyro + this.gyro.ToString();
+            this.testMeshPro.text += "\n";
+            this.testMeshPro.text += acc + this.acc.ToString();
+
+        }
+    }
+
+}
---
+++ zumo/Assets/Scripts/HakoAssets/HakoAssets/PluggableAsset/Assets/Robot/Zumo/ZumoMotorController.cs
@@ -0,0 +1,96 @@
+using Hakoniwa.PluggableAsset.Assets.Robot.Parts;
+using Hakoniwa.PluggableAsset.Communication.Connector;
+using Hakoniwa.PluggableAsset.Communication.Pdu;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hakoniwa.PluggableAsset.Assets.Robot.Zumo
+{
+    enum ZumoMotorType
+    {
+        MotorType_Left = 0,
+        MotorType_Right,
+        MotorType_Num
+    }
+
+    public class ZumoMotorController : MonoBehaviour, IRobotPartsController
+    {
+        private GameObject root;
+        private string root_name;
+        private PduIoConnector pdu_io;
+        private IPduReader pdu_reader;
+        private IRobotPartsMotor[] front_motors = new IRobotPartsMotor[(int)ZumoMotorType.MotorType_Num];
+        private IRobotPartsMotor[] back_motors = new IRobotPartsMotor[(int)ZumoMotorType.MotorType_Num];
+
+        public void Initialize(object obj)
+        {
+            GameObject tmp = null;
+            try
+            {
+                tmp = obj as GameObject;
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.LogError("Initialize error: " + e.Message);
+                return;
+            }
+
+            if (this.root == null)
+            {
+                this.root = tmp;
+                this.root_name = string.Copy(this.root.transform.name);
+                this.pdu_io = PduIoConnector.Get(root_name);
+                if (this.pdu_io == null)
+                {
+                    throw new ArgumentException("can not found pdu_io:" + root_name);
+                }
+                var pdu_reader_name = root_name + "_" + "actuatorPdu";
+                this.pdu_reader = this.pdu_io.GetReader(pdu_reader_name);
+                if (this.pdu_reader == null)
+                {
+                    throw new ArgumentException("can not found pdu_reader:" + pdu_reader_name);
+                }
+                this.front_motors[(int)ZumoMotorType.MotorType_Left] = this.transform.Find("Front").Find("Left").GetComponentInChildren<IRobotPartsMotor>();
+                this.front_motors[(int)ZumoMotorType.MotorType_Right] = this.transform.Find("Front").Find("Right").GetComponentInChildren<IRobotPartsMotor>();
+                this.back_motors[(int)ZumoMotorType.MotorType_Left] = this.transform.Find("Back").Find("Left").GetComponentInChildren<IRobotPartsMotor>();
+                this.back_motors[(int)ZumoMotorType.MotorType_Right] = this.transform.Find("Back").Find("Right").GetComponentInChildren<IRobotPartsMotor>();
+
+            }
+            for (int i = 0; i < this.front_motors.Length; i++)
+            {
+                if (this.front_motors[i] != null)
+                {
+                    this.front_motors[i].Initialize(root);
+                }
+            }
+            for (int i = 0; i < this.back_motors.Length; i++)
+            {
+                if (this.back_motors[i] != null)
+                {
+                    this.back_motors[i].Initialize(root);
+                }
+            }
+        }
+
+        public void DoControl()
+        {
+            var left_power = this.pdu_reader.GetReadOps().Refs("motors")[(int)ZumoMotorType.MotorType_Left].GetDataInt32("power");
+            var right_power = this.pdu_reader.GetReadOps().Refs("motors")[(int)ZumoMotorType.MotorType_Right].GetDataInt32("power");
+
+
+            this.front_motors[(int)ZumoMotorType.MotorType_Left].SetTargetVelicty((float)left_power);
+            this.front_motors[(int)ZumoMotorType.MotorType_Right].SetTargetVelicty((float)right_power);
+
+            this.back_motors[(int)ZumoMotorType.MotorType_Left].SetTargetVelicty((float)left_power);
+            this.back_motors[(int)ZumoMotorType.MotorType_Right].SetTargetVelicty((float)right_power);
+        }
+
+        public RosTopicMessageConfig[] getRosConfig()
+        {
+            return null;
+        }
+    }
+
+}
