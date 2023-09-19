c35d7663e8630b0b2f8cde1081a14fcda9389e3c
Takashi Mori <tmori@esm.co.jp>
2023-09-19 12:21:38 +0900

    bug fixed

--- update.bash
+++ update.bash
@@ -4,7 +4,7 @@
  cp -rp plugin/plugin-srcs/Assets/Scripts/HakoAssets zumo/Assets/Scripts/HakoAssets/
 
  # Prefabs
- cp -rp plugin/plugin-srcs/Assets/Prefab/Robots/ZumoRobo*.prefab* zumo/Assets/Prefab/
+ cp -rp plugin/plugin-srcs/Assets/Prefab/Robots/ZumoRobo*.prefab* zumo/Assets/Prefab/Robots/
 
  # Scenes
  cp plugin/plugin-srcs/Assets/Scenes/Zumo/* zumo/Assets/Scenes/Zumo/
