f61d2574de86c649f5cbcd33d4456c465d47940e
Takashi Mori <kanetugu2015@gmail.com>
2023-09-19 11:35:01 +0900

    updated for latest

--- install.bash
+++ install.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+
 if [ -d plugin/plugin-srcs ]
 then
-    :
+    if [ ! -d plugin/plugin-srcs/Assets/Model/Zumo -a ! -f zumo/Assets/Model/Zumo ]
+    then
+        echo "ERROR can not found Zumo.zip"
+        echo "please download here: https://github.com/ncesnagoya/hakoniwa-unity-zumo/releases/tag/v1.1.0"
+        echo "and unzip Zumo.zip on the zumo/Assets/Model, "
+        echo "after that, please remove Zumo.zip"
+        exit 1
+    fi
 else
     echo "ERROR: can not find submodule plugin"
     echo "git pull"
@@ -16,5 +24,3 @@ bash install.bash
 cd ..
 
 cp -rp zumo/Assets/* plugin/plugin-srcs/Assets/
-cp -rp zumo/ros_types/* plugin/plugin-srcs/ros_types/
-cp zumo/hakoniwa_path.json plugin/plugin-srcs/
